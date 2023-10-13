9f1118190a287def5fff62952dae8c40f5a0ac46
Pasquale Muraca <70173028+Pasqualecoder@users.noreply.github.com>
2023-10-13 22:06:35 +0200

    Add files via upload

---
+++ TODO.md
@@ -0,0 +1,8 @@
+# To-do list
+
+- ### show status
+pinned a message on the top, the bot modifies the text
+once a minute showing the current time. Doing so if
+the showed time is different from the current time it
+means that the bot is offline.
+
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import datetime
 import time
 import threading
+import mimetypes
 import git
 import telebot
 from dotenv import dotenv_values
@@ -15,16 +16,18 @@ chat_id = env_data['CHAT_ID']
 refresh_delay = int(env_data['REFRESH_DELAY'])
 repository_path = env_data['REPOSITORY_PATH']
 last_commit_date = env_data['LAST_COMMIT_DATE']
-
-
 bot = telebot.TeleBot(BOT_TOKEN)
-
-print("I'm alive!")
 bot.send_message(chat_id, "I'm alive!")
 
 
+# log to console
+def log(val):
+    # FIXME: [<class 'datetime.datetime'>] I'm alive!
+    print("[" + str(datetime.datetime) + "] " + str(val))
+
+
 def start_commit_updater():
-    # Esegui la funzione in un thread separato
+    # run check_commit() in a separate thread
     periodic_thread = threading.Thread(target=check_commit)
     periodic_thread.daemon = True
     periodic_thread.start()
@@ -40,7 +43,13 @@ def check_commit():
     repo_name = repo.remotes.origin.url.split('.git')[0].split('/')[-1]
 
     while True:
-        result = g.pull()
+        try:
+            result = g.pull()
+        except Exception as failed_commit:
+            print(failed_commit)
+            time.sleep(refresh_delay)
+            continue
+
         commit_date = datetime.datetime.fromtimestamp(master.commit.committed_date)
         commit_message = master.commit.message
         commit_author = master.commit.author.name
@@ -55,11 +64,51 @@ def check_commit():
         time.sleep(refresh_delay)
 
 
-# Gestore per il comando /ping
+# /ping command handler
 @bot.message_handler(commands=['ping'])
 def ping_pong(message):
     bot.send_message(message.chat.id, "pong")
 
 
-start_commit_updater()
-bot.polling(interval=5)
+# /cat command handler
+@bot.message_handler(commands=['cat'])
+def cat_file(message):
+    try:
+        file_path = message.text.split()[1]  # get file_path
+        file_path = repository_path + "/" + file_path
+        if len(file_path) < 2:
+            response = "Meow, specify the full path to the file in the repo"
+
+        # check if the file exists
+        elif not os.path.exists(file_path):
+            response = "The file doesn't exist"
+
+        else:
+            # get the MIME type of the file (_ ignores the rest of the result)
+            mime_type, _ = mimetypes.guess_type(file_path)
+
+            # check if the MIME type is a text type
+            if mime_type and mime_type.startswith('text'):
+                # writes the file into the message text
+                with open(file_path, 'r') as filereader:
+                    response = filereader.read()
+            else:
+                response = "This is not a plain text file and cannot be displayed."
+
+    except Exception as read_file_error:
+        response = "An error occurred: " + str(read_file_error)
+        log(response)
+
+    bot.reply_to(message, response)
+
+
+if __name__ == '__main__':
+    log("I'm alive!")
+    start_commit_updater()
+
+    while True:
+        try:
+            bot.polling(interval=5)
+        except Exception as connection_timeout:
+            print(str(datetime.datetime) + str(connection_timeout))
+            time.sleep(refresh_delay)
